e991cb7eeb49b9ff342850a807fc4f164978b467
orionSX <asiskrich@gmail.com>
2024-09-13 19:34:36 +0300

    executing commadns from input

--- getting_started.rb
+++ getting_started.rb
@@ -19,3 +19,16 @@ else
 	puts "Never heard about that one 0_0\n"
 end
 puts "Whatever, soon it'll be Ruby anyways"
+
+puts "\nEnter rubby command:"
+ruby_command = STDIN.gets.chomp
+puts "\nEnter OS command:"
+os_command = STDIN.gets.chomp
+
+puts "Result of Ruby command :"
+puts eval(ruby_command)
+if system(os_command)
+	puts "OS command executed"
+else
+	puts "Something went wrong"
+end
