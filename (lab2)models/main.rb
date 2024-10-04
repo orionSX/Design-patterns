cc92bb3b542f21ec71a65d263a497d98fc1de821
orionSX <asiskrich@gmail.com>
2024-10-04 17:01:35 +0300

    lab2 first_task final_form

--- (lab2)models/main.rb
+++ (lab2)models/main.rb
@@ -1,13 +1,16 @@
 require_relative "student_model"
 st4_info={
-    surname:"ZXC",first_name:"ASD",last_name:"FFFF",email:"chXDXDXDeta@gde.to",phone: "12345678910"
+    surname:"ZXC",first_name:"ASD",last_name:"FFFF",phone: "12345678910",git:"https://github.com/orionSX"
 }
 
 st1=Student.new(surname:"A",first_name:"B",last_name:"C")
 
 st2=Student.new(surname:"SDASDAS",first_name:"E",last_name:"F",email:"cheta@gde.to")
-st3=Student.new(surname:"D",first_name:"E",last_name:"F",email:"cheta@gde.to",phone: "")
+st3=Student.new(surname:"D",first_name:"E",last_name:"F",email:"cheta@gde.to")
 st4=Student.new(**st4_info)
+
 puts st1.to_s
 puts "--------"
+puts st4.to_s
+puts st4.set_contacts(phone:"1234567899")
 puts st4.to_s
--- (lab2)models/student_model.rb
+++ (lab2)models/student_model.rb
@@ -1,79 +1,84 @@
-module StudentValidator
 
 
+class StudentValidator
+  NAME_REGEX= /\A[a-zA-Zа-яА-ЯёЁ'-]{1,50}\z/
+  PHONE_REGEX=  /\A\+?\d{10,15}\z/
+  EMAIL_REGEX= /\A[\w\-.]+@[a-zA-Z\d\-.]+\.[a-zA-Z]{2,}\z/
+  GIT_REGEX=/\A(https:\/\/)?(www\.)?github.com\/[a-zA-Z0-9_-]+\z/
+  TG_REGEX=/\A@[a-zA-Z0-9_]{5,32}\z/
 
-  def phone_valid?(phone)
-    !!(phone.to_s =~ /\A\+?\d{10,15}\z/) || phone.nil? || phone.to_s==""
+  def valid?(value, reg)
+    raise('Reg should be a regex') unless reg.is_a?(Regexp)
+    !!(value.to_s =~ reg)
   end
-
-  def fio_valid?(fname,sname,lname)
-    !!(fname.to_s =~  /\A[a-zA-Zа-яА-ЯёЁ'-]{1,50}\z/)&&!!(sname.to_s =~  /\A[a-zA-Zа-яА-ЯёЁ'-]{1,50}\z/)&&!!(lname.to_s =~  /\A[a-zA-Zа-яА-ЯёЁ'-]{1,50}\z/)
-  end
-
-  def email_valid?(email)
-
-    !!(email.to_s =~  /\A[\w\-.]+@[a-zA-Z\d\-.]+\.[a-zA-Z]{2,}\z/) || email.nil? || email.to_s==""
-  end
-
-  def git_valid?(git)
-
-    !!(git.to_s=~/\A(https:\/\/)?(www\.)?github.com\/[a-zA-Z0-9_-]\z/) || git.nil? || git.to_s==""
-  end
-
-  def tg_valid?(tg)
-    !!(tg.to_s=~/\A@[a-zA-Z0-9_]{5,32}\z/) || tg.nil? || tg.to_s==""
-  end
-
-  
 end
 
-class Student
-  extend StudentValidator
+class Student < StudentValidator
+  @@id = 0
 
-  @@id=0
+  attr_reader :id, :email, :phone, :tg, :git
 
-  attr_accessor :id,:surname,:first_name,:last_name,:phone,:tg,:email,:git
- 
-
-  def initialize(first_name:,surname:,last_name:,phone: nil,tg: nil, git: nil,email:nil)
-    self.set_fio(first_name,surname,last_name)
-    self.phone=phone.to_s
-    self.tg=tg.to_s
-    self.git=git.to_s
-    self.email=email.to_s
-    self.id=@@id
-    @@id+=1
-
+  def initialize(first_name:, surname:, last_name:,phone: nil, tg: nil, git: nil, email: nil)
+    set_fio(first_name, surname, last_name)
+    set_contacts(phone: phone, tg: tg, git: git, email: email)
+    @id = @@id
+    @@id += 1
   end
 
   def to_s
     instance_variables.map do |key|
       val = instance_variable_get(key)
-     
-      key_name = key.to_s.delete_prefix('@') 
-      "#{key_name} : #{val}" unless val.nil? || val ==""
-    end.compact.join("\n") 
+      key_name = key.to_s.delete_prefix('@')
+      "#{key_name}: #{val}" unless val.nil? || val.to_s.empty?
+    end.compact.join("\n")
   end
 
-  def email=(value)    
-    
-    Student.email_valid?(value) ?  @email=value.to_s : raise("Incorrect email #{value}")
+  def validate
+    (valid?(@git, GIT_REGEX) && !@git.nil?) &&
+    (valid?(@email, EMAIL_REGEX) && !@email.nil? || valid?(@phone, PHONE_REGEX) && !@phone.nil? || valid?(@tg, TG_REGEX) && !@tg.nil?)
   end
-  def tg=(value)    
-    Student.tg_valid?(value) ?  @tg=value.to_s : raise("Incorrect tg #{value}")
-  end
-  def git=(value)    
-    Student.git_valid?(value) ?  @git=value.to_s : raise("Incorrect git #{value}")
-  end
-  def phone=(value)    
-    Student.phone_valid?(value) ?  @phone=value.to_s : raise("Incorrect phone #{value}")
+ 
+  def set_contacts(phone: nil , tg: nil, git:nil, email:nil)
+    set_contact(:phone, phone) if phone
+    set_contact(:tg, tg) if tg
+    set_contact(:git, git) if git
+    set_contact(:email, email) if email
   end
 
-  def set_fio(first_name,surname,last_name)    
-    Student.fio_valid?(first_name,surname,last_name) ?  [@first_name=first_name.to_s , @surname=surname.to_s , @last_name=last_name.to_s]  : raise("Incorrect fio")
+  private
+
+  # Приватные сеттеры для каждого контакта
+  def set_contact(contact_name, value)
+    regex_map = {
+      phone: PHONE_REGEX,
+      tg: TG_REGEX,
+      git: GIT_REGEX,
+      email: EMAIL_REGEX
+    }
+
+    regex = regex_map[contact_name]
+    raise "Validation not defined for #{contact_name}" unless regex
+
+    if valid?(value, regex)
+      instance_variable_set("@#{contact_name}", value.to_s)
+    else
+      raise "Incorrect #{contact_name}: #{value}"
+    end
   end
 
+  # Приватный метод для установки ФИО
+  def set_fio(first_name, surname, last_name)
+    set_namestr(:first_name, first_name)
+    set_namestr(:surname, surname)
+    set_namestr(:last_name, last_name)
+  end
 
+  # Приватный метод для установки атрибута с валидацией
+  def set_namestr(attr_name, value)
+    if valid?(value, NAME_REGEX)
+      instance_variable_set("@#{attr_name}", value.to_s)
+    else
+      raise "Incorrect #{attr_name}: #{value}"
+    end
+  end
 end
-
-
