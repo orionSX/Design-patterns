13d3eaacbd66459c5a1dd630ecf355f6d2ba409f
orionSX <asiskrich@gmail.com>
2024-10-04 16:10:39 +0300

    lab2 task8 validator module

--- (lab2)models/main.rb
+++ (lab2)models/main.rb
@@ -1,9 +1,13 @@
 require_relative "student_model"
-
+st4_info={
+    surname:"ZXC",first_name:"ASD",last_name:"FFFF",email:"chXDXDXDeta@gde.to",phone: "12345678910"
+}
 
 st1=Student.new(surname:"A",first_name:"B",last_name:"C")
 
-st2=Student.new(surname:"D",first_name:"E",last_name:"F",email:"cheta@gde.to",phone: "")
+st2=Student.new(surname:"SDASDAS",first_name:"E",last_name:"F",email:"cheta@gde.to")
+st3=Student.new(surname:"D",first_name:"E",last_name:"F",email:"cheta@gde.to",phone: "")
+st4=Student.new(**st4_info)
 puts st1.to_s
 puts "--------"
-puts st2.to_s
+puts st4.to_s
--- (lab2)models/student_model.rb
+++ (lab2)models/student_model.rb
@@ -1,32 +1,76 @@
-class Student
-  attr_accessor :id,:surname,:first_name,:last_name,:phone,:tg,:email,:git
+module StudentValidator
 
-  def initialize(first_name:,surname:,last_name:,phone: nil,tg: nil, git: nil,email:nil,id:nil)
-    self.first_name=first_name
-    self.surname=surname
-    self.last_name=last_name
-    self.phone=phone
-    self.tg=tg
-    self.git=git
-    self.email=email
-    self.id=id
+
+
+  def phone_valid?(phone)
+    !!(phone.to_s =~ /\A\+?\d{10,15}\z/) || phone.nil? || phone.to_s==""
+  end
+
+  def fio_valid?(fname,sname,lname)
+    !!(fname.to_s =~  /\A[a-zA-Zа-яА-ЯёЁ'-]{1,50}\z/)&&!!(sname.to_s =~  /\A[a-zA-Zа-яА-ЯёЁ'-]{1,50}\z/)&&!!(lname.to_s =~  /\A[a-zA-Zа-яА-ЯёЁ'-]{1,50}\z/)
+  end
+
+  def email_valid?(email)
+
+    !!(email.to_s =~  /\A[\w\-.]+@[a-zA-Z\d\-.]+\.[a-zA-Z]{2,}\z/) || email.nil? || email.to_s==""
+  end
+
+  def git_valid?(git)
+
+    !!(git.to_s=~/\A(https:\/\/)?(www\.)?github.com\/[a-zA-Z0-9_-]\z/) || git.nil? || git.to_s==""
+  end
+
+  def tg_valid?(tg)
+    !!(tg.to_s=~/\A@[a-zA-Z0-9_]{5,32}\z/) || tg.nil? || tg.to_s==""
+  end
+
+  
+end
+
+class Student
+  extend StudentValidator
+
+  @@id=0
+
+  attr_accessor :id,:surname,:first_name,:last_name,:phone,:tg,:email,:git
+ 
+
+  def initialize(first_name:,surname:,last_name:,phone: nil,tg: nil, git: nil,email:nil)
+    self.set_fio(first_name,surname,last_name)
+    self.phone=phone.to_s
+    self.tg=tg.to_s
+    self.git=git.to_s
+    self.email=email.to_s
+    self.id=@@id
+    @@id+=1
+
   end
 
   def to_s
     instance_variables.map do |key|
       val = instance_variable_get(key)
+     
       key_name = key.to_s.delete_prefix('@') 
-      "#{key_name} : #{val}" unless val.nil? 
+      "#{key_name} : #{val}" unless val.nil? || val ==""
     end.compact.join("\n") 
   end
 
-  def Student.is_phone?(phone)
-    !!(phone.to_s =~ /\A\+?\d{10,15}\z/)    
+  def email=(value)    
+    
+    Student.email_valid?(value) ?  @email=value.to_s : raise("Incorrect email #{value}")
+  end
+  def tg=(value)    
+    Student.tg_valid?(value) ?  @tg=value.to_s : raise("Incorrect tg #{value}")
+  end
+  def git=(value)    
+    Student.git_valid?(value) ?  @git=value.to_s : raise("Incorrect git #{value}")
+  end
+  def phone=(value)    
+    Student.phone_valid?(value) ?  @phone=value.to_s : raise("Incorrect phone #{value}")
   end
 
-  def phone=(value) 
-    @phone=nil   
-    Student.is_phone?(value) ?  @phone=value.to_s : raise("Incorrect phone #{value}") unless value.nil? || value.to_s==""
+  def set_fio(first_name,surname,last_name)    
+    Student.fio_valid?(first_name,surname,last_name) ?  [@first_name=first_name.to_s , @surname=surname.to_s , @last_name=last_name.to_s]  : raise("Incorrect fio")
   end
 
 
