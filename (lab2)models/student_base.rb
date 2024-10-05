64b1848135e365ed3698ff042d59903cddd04b85
Orion <asiskrich@gmail.com>
2024-10-05 10:13:38 +0300

    Update student_base.rb

--- (lab2)models/student_base.rb
+++ (lab2)models/student_base.rb
@@ -1,1 +1,58 @@
+require_relative "student_validator.rb"
 
+class StudentBase
+include StudentValidator
+
+  attr_reader :id,:git
+
+  def initialize(id:nil,git:nil)
+    @id=id.to_i
+    set_attribute(:git,git) if git
+  end
+  
+  def validate
+    s=instance_variables.map do |key|
+      val = instance_variable_get(key)
+      key_name = key.to_s.delete_prefix('@')
+      if key_name=="contact"||key_name=='phone'||key_name=='email'||key_name=='tg' && !val.to_s.empty?
+        
+        val
+      else
+          nil
+      end
+    end.compact.join("")
+
+    !@git.nil? &&( s.length!=0)
+  end
+  def to_s
+    instance_variables.map do |key|
+      val = instance_variable_get(key)
+      key_name = key.to_s.delete_prefix('@')
+      "#{key_name}: #{val}" unless val.nil? || val.to_s.empty?
+    end.compact.join("\n")
+  end
+  private
+  def set_attribute(attr_name, value)
+    regex_map = {
+      email: EMAIL_REGEX,
+      tg: TG_REGEX,
+      git: GIT_REGEX,
+      phone: PHONE_REGEX,
+      first_name: NAME_REGEX,
+      surname: NAME_REGEX,
+      last_name: NAME_REGEX,
+      contact: ANY_REGEX,
+      fio:ANY_REGEX,
+    }
+
+    regex = regex_map[attr_name]
+    raise "Validation not defined for #{attr_name}" unless regex
+
+    if valid?(value, regex)
+      instance_variable_set("@#{attr_name}", value.to_s)
+    else
+      raise "Incorrect #{attr_name}: #{value}"
+    end
+  end
+ 
+end
