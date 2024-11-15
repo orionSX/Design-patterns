2c4433ec2eef2fef8656411630297dbb0e7cada9
orion <asiskrich@gmail.com>
2024-11-16 00:04:31 +0300

    lab4 task2: dataset + lil bit of testing

--- student_models/main.rb
+++ student_models/main.rb
@@ -1,27 +1,13 @@
 require_relative "student/student_model.rb"
 require_relative "student/student_short.rb"
 require_relative "student/BST.rb"
-students = [
-  { surname: "A", first_name: "B", last_name: "C", date_of_birth: '10.03.2000' },
-  { surname: "SDASDAS", first_name: "E", last_name: "F", date_of_birth: '25.06.2002' },
-  { surname: "D", first_name: "E", last_name: "F", date_of_birth: '15.08.1998' },
-  { surname: "ZXC", first_name: "ASD", last_name: "FFFF", date_of_birth: '30.03.2004' },
-  { surname: "QWE", first_name: "Q", last_name: "R", date_of_birth: '12.12.2001' },
-  { surname: "RTY", first_name: "T", last_name: "Y", date_of_birth: '11.11.1997' },
-  { surname: "ASD", first_name: "S", last_name: "D", date_of_birth: '20.02.2000' },
-  { surname: "FGH", first_name: "F", last_name: "H", date_of_birth: '22.10.1999' },
-  { surname: "VBN", first_name: "V", last_name: "B", date_of_birth: '15.04.1995' },
-  { surname: "QAZ", first_name: "Z", last_name: "X", date_of_birth: '17.07.2003' }
-]
+require_relative 'student/file_strategy/file_json.rb'
+require_relative 'student/file_strategy/file_yaml.rb'
+require_relative "student/student_list/student_list_base.rb"
 
 
-student_objects = students.map do |info|
-  student = Student.new(**info)
- 
-end
-tree = BST.new
-student_objects.each do |student|
-   
-   tree.add_data(student)
- end
-tree.each(type: :BFS){|x| puts x.date_of_birth}
+
+json_list=StudentsListBase.new('students.json',JSONStrategy)
+yaml_list=StudentsListBase.new('students.yaml',YAMLStrategy)
+yaml_list.delete_student(1)
+puts yaml_list.students
