758a715f549e701d681f945c464cd5df6a04aa10
orion <asiskrich@gmail.com>
2024-11-30 06:03:05 +0300

    main quickfix

--- student_models/main.rb
+++ student_models/main.rb
@@ -3,6 +3,6 @@ require_relative "student/student_short.rb"
 require_relative "student/BST.rb"
 require_relative 'student/file_strategy/file_json.rb'
 require_relative 'student/file_strategy/file_yaml.rb'
-require_relative "student/student_list/student_list_base.rb"
+require_relative "student/student_list/student_list.rb"
 require 'mysql2'
 
